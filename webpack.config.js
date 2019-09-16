90d8ddda9289067afba508c62d2567051474bddb
Serhan Coşkun <grafikri@gmail.com>
2019-09-16 12:25:29 +0300

    webpack added to expose bundle file

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path")
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    library: "YouTube",
+    libraryTarget: "umd"
+  },
+  module: {
+    rules: [{ test: /\.ts$/, use: "ts-loader", exclude: /node_modules/ }]
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  mode: "development"
+}
